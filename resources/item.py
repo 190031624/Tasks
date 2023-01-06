918f18310b6a40fc91436051bc580b802d7d66e3
ar-thota.kumar <ar-thota.kumar@rakuten.com>
2023-01-06 16:17:27 +0530

    first commit

---
+++ resources/item.py
@@ -0,0 +1,52 @@
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from models import ItemModel
+from schemas import ItemSchema, ItemUpdateSchema
+from db import db
+from sqlalchemy.exc import SQLAlchemyError
+
+blp = Blueprint("items", __name__, description="Operations on items")
+
+@blp.route("/item")
+class Items(MethodView):
+    @blp.response(200,ItemSchema(many=True))
+    def get(self):
+        return ItemModel.query.all()
+        
+    @blp.arguments(ItemSchema)
+    @blp.response(201,ItemSchema)
+    def post(self,item_data):
+        item=ItemModel(**item_data)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500,message="an error occured while inserting an item.")
+        return item
+
+@blp.route("/item/<string:item_id>")
+class Items_list(MethodView):
+    @blp.response(200,ItemSchema)
+    def get(self,item_id):
+        item=ItemModel.query.get_or_404(item_id)
+        return item
+        
+    def delete(self,item_id):
+        item=ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message":"item deleted "}
+
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200,ItemSchema)
+    def put(self,item_data,item_id):
+        item=ItemModel.query.get_or_404(item_id)
+        if item:
+            item.name=item_data["name"]
+            item.price=item_data["price"]
+        else:
+            new_item=ItemModel(id=item_id**item_data)
+        db.session.add(item)
+        db.commit() 
+        return item
